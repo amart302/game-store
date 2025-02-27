e901be79fa8f5332dc643591d4e90827a5e74353
Мартазанов Ахмед А <MartazanovAA23@ithub.local>
2025-02-27 14:58:30 +0300

    commit 10

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+app.get("/api/steam/:appId", async (req, res) => {
+    try {
+        const gameId = req.params.appId;
+        const responce = await axios.get(`https://store.steampowered.com/api/appdetails?appids=${gameId}`);
+        res.json(responce.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch data from Steam API' });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Сервер запущен на http://localhost:3000");
+});
