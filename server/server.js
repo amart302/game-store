3b145d96b202da2aef642a90b0be6cc0e50db84d
Ahmed <amartazanov892@gmail.com>
2025-03-12 17:09:23 +0300

    commit 40

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-const fs = require("fs");
-const path = require("path");
+import express from "express";
+import axios from "axios";
+import cors from "cors";
 
 const app = express();
 
@@ -19,30 +17,6 @@ app.get("/api/steam/:appId", async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch data from Steam API' });
     }
-});
-
-function saveBase64Image(base64String, outputPath) {
-    const base64Data = base64String.replace(/^data:image\/\w+;base64,/, '');
-    const buffer = Buffer.from(base64Data, 'base64');
-    fs.writeFileSync(outputPath, buffer);
-}
-
-const dir = path.resolve(__dirname, '../client/src/assets/uploads');
-
-const outputFilePath = path.join(dir, `${Date.now()}.png`);
-
-if(!fs.existsSync(dir)){
-    fs.mkdirSync(dir, { recursive: true });
-}
-
-app.post("/upload", (req, res) => {
-    console.log(req);
-    
-    if(!req.file){
-        return res.status(400).send('Нет файла');
-    }
-    const base64Image = req.file;
-    saveBase64Image(base64Image, outputFilePath);
 });
 
 app.listen(3000, () => {
