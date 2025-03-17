98fc8e74506b5bd0ac3a5347f17013e4f4e99047
amart <amartazanov892@gmail.com>
2025-03-17 09:30:04 +0300

    commit 41.2

--- client/src/pages/NotFound.vue
+++ client/src/pages/NotFound.vue
@@ -1,8 +1,33 @@
 <template>
-    <h1 style="color: white;">404 :(</h1>
+    <Header />
+    <main>
+        <h1 style="color: white;">404 :(</h1>
+    </main>
+    <Footer />
 </template>
-<style>
-h1{
-    font-size: 60px;
-}
+<script>
+    import Footer from '@/components/Footer.vue';
+    import Header from '@/components/Header.vue';
+
+    export default {
+        components: {
+            Header,
+            Footer
+        }
+    }
+</script>
+<style scoped>
+/* 212  */
+    main {
+        max-width: 1440px;
+        min-height: calc(100vh - 600px);
+        margin: 0 auto;
+        padding: 60px 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    h1{
+        font-size: 70px;
+    }
 </style>
